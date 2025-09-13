69edebf6213c22827c44f1bd4860aae28bb8263a
habeeb-codeman <148739211+habeeb-codeman@users.noreply.github.com>
2025-09-13 14:15:50 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
-            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/product" element={<Gallery />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
